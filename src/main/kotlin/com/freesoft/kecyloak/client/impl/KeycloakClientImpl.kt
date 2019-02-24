9eb91d3ae8420464d68980dab3522abd4ab9169e
bogdan.dina <bogdan.dina@orange.com>
2019-02-24 22:25:17 +0200

    redesign keycloak client impl

--- src/main/kotlin/com/freesoft/kecyloak/client/impl/KeycloakClientImpl.kt
+++ src/main/kotlin/com/freesoft/kecyloak/client/impl/KeycloakClientImpl.kt
@@ -3,9 +3,8 @@ package com.freesoft.kecyloak.client.impl
 import com.freesoft.kecyloak.client.api.KeycloakClient
 import com.freesoft.kecyloak.client.api.KeycloakClientConfig
 
-class KeycloakClientImpl(private val config: KeycloakClientConfig,
-                         private val keycloakApi: KeycloakApi) : KeycloakClient {
+class KeycloakClientImpl(private val config: KeycloakClientConfig) : KeycloakClient {
 
 
-    override fun customerServices() = CustomerServicesImpl.aCustomerServices(keycloakApi)
+    override fun customerServices() = CustomerServicesImpl.aCustomerServices(KeycloakApi(config))
 }
